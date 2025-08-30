ef2f23d27848a30ac12aedc1a90e008a37182dfa
joseph <josephflec@gmail.com>
2025-08-30 18:13:04 -0500

    speed insight update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "./styles/reset.css";
 import "./styles/base.css";
 
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
