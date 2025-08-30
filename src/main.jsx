f9269f18b6664ffbc079f73fa54b8d28b4c0ae27
joseph <josephflec@gmail.com>
2025-08-30 18:16:28 -0500

    final test

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,8 @@ import App from "./App.jsx";
 import "./styles/reset.css";
 import "./styles/base.css";
 
-import { SpeedInsights } from "@vercel/speed-insights/react";
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
   </React.StrictMode>
 );
